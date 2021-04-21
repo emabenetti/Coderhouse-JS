66014f8517789842372c8040af474e9da0980596
Emanuel Benetti <emanuelbenetti39@gmail.com>
2021-04-21 14:04:35 -0300

    Terminada clase 9

--- Clase 9 - Eventos/JS/main.js
+++ Clase 9 - Eventos/JS/main.js
@@ -9,3 +9,17 @@ mostrarArmas();
 
 //Se crea una nueva arma
 const lanza = new Arma("Lanza", "Windbreaker", 280, "Lanza utilizada por el Dios Aqua en su guerra contra el dios del viento.")
+
+//se definen los botones en una variable
+let botonSi = document.getElementById("bEquip");
+let botonNo = document.getElementById("bElim");
+
+//se les asigna una funcion al evento onclick de cada boton
+botonSi.onclick = agregarElemento;
+botonNo.onclick = eliminarElemento;
+
+//variable global para guardar la posición del arma mostrada en pantalla (índice del array)
+let pos = 0;
+
+//asignamos una funcion al evento de presionar cualquier tecla
+document.onkeydown = detectaFlechas;
--- Clase 9 - Eventos/JS/scripts.js
+++ Clase 9 - Eventos/JS/scripts.js
@@ -15,23 +15,28 @@ class Arma {
 function mostrarArmas() {
     //Identifico el elemento padre <ul>
     let padre = document.getElementById("listaArmas");
-    
+
     //Esto es para borrar el contenido de <ul> 
     //por que si llamas varias veces a la funcion va a agregar todos los elementos de nuevo como <li>
     //esto asegura que lo mostrado siempre es SOLAMENTE lo que tiene adentro el array
     padre.innerHTML = "";
 
-    //un for para recorrer el array y agregar los elementos
-    for (const arma of inventario){
-        let li = document.createElement("li");
-        li.innerHTML = arma.nombre + " (" + arma.tipo + ")";
-        padre.appendChild(li);
-    }
+    //Se crean 3 elementos li iniciales. La funcion detectaTeclas se va a encargar de cambiar el elemento li del arma, dejando las flechas sin tocar
+    let li = document.createElement("li");
+    let izq = document.createElement("izq");
+    let der = document.createElement("der");
+    li.innerHTML = inventario[0].nombre + " (" + inventario[0].tipo + ")";
+    izq.innerHTML = "< - ";
+    der.innerHTML = " - >";
+
+    padre.appendChild(izq);
+    padre.appendChild(li);
+    padre.appendChild(der);
 }
 
 //Funcion simple para agregar un elemento al final de un array
-function agregarElemento(elemento) {
-    inventario.push(elemento);
+function agregarElemento() {
+    inventario.push(lanza);
     //Elimina el div para que no pueda seguir agregando la misma arma
     eliminarElemento();
     //Finalmente muestra el nuevo array
@@ -47,4 +52,28 @@ function eliminarElemento() {
     let div = document.getElementById("encuentro");
     //Elimino el div haciendo referencia a su elemento padre.
     div.parentNode.removeChild(div);
+}
+
+//funcion que detecta el evento de teclas presionada y realiza acciones si esas teclas eran la flecha izq o der
+function detectaFlechas(e) {
+    //guardo en una variable el elemento li creado en la función mostrarArmas()
+    let lista = document.getElementsByTagName("li");
+    switch (e.keyCode) {
+        //37 es el keycode para la flecha izq
+        case 37:
+            //se comprueba que no restemos por debajo de cero, no existen indices negativos en el array
+            if (pos > 0) {
+                pos--;
+                lista[0].innerHTML = inventario[pos].nombre + " (" + inventario[pos].tipo + ")";
+            }
+            break;
+        //39 es el keycode para la flecha der
+        case 39:
+            //se comprueba que no sumemos por encima del ultimo objeto del array
+            if (pos < inventario.length-1) {
+                pos++;
+                lista[0].innerHTML = inventario[pos].nombre + " (" + inventario[pos].tipo + ")";
+            }
+            break;
+    }
 }
